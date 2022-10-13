64f81ecb6fc7e12059ebfda46b31251759f47cf5
Marco Tröster <marco@troester-gmbh-de>
2022-10-13 21:01:33 +0200

    extend benchmark with profiler for optimizations

--- simulation_benchmark_zero_load.py
+++ simulation_benchmark_zero_load.py
@@ -1,14 +1,15 @@
+from cProfile import Profile
+from pstats import Stats
 from robot_sf.robot_env import RobotEnv
 
 
-def main():
-    total_steps = 1000
+def benchmark():
+    total_steps = 20000
     env = RobotEnv(difficulty=2)
     obs = env.reset()
 
     peds_sim = env.sim_env
-    print(peds_sim.max_population_for_new_individual)
-    print(peds_sim.max_population_for_new_group)
+    print('start of simulation')
 
     episode = 0
     ep_rewards = 0
@@ -16,13 +17,26 @@ def main():
         rand_action = env.action_space.sample()
         obs, reward, done, _ = env.step(rand_action)
         ep_rewards += reward
-        print(f'step {step}, reward {reward} (peds: {peds_sim.peds.size()})')
+        # print(f'step {step}, reward {reward} (peds: {peds_sim.peds.size()})')
 
         if done:
             episode += 1
             print(f'end of episode {episode}, total rewards {ep_rewards}')
+            ep_rewards = 0
             obs = env.reset()
+
+    print('end of simulation')
  
+
+def main():
+    profiler = Profile()
+    profiler.enable()
+    benchmark()
+    profiler.disable()
+
+    stats = Stats(profiler).sort_stats('tottime')
+    stats.print_stats(50)
+
 
 if __name__ == "__main__":
     main()

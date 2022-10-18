6af274f5ac4602dc126b15e7e9abbb538ba23e94
Marco Tröster <marco@troester-gmbh.de>
2022-10-18 05:25:04 +0200

    refactor occupancy grid (wip) - simplify instance members - make occupancy_overall a computed property - simplify position_bounds - compute coords of meshgrid with helper functions - inline inflate logic

--- robot_sf/map.py
+++ robot_sf/map.py
@@ -1,158 +1,143 @@
+from math import floor
 from typing import Callable, Tuple, List
 
 import numpy as np
 
 from robot_sf.vector import Vec2D
-from robot_sf.utils.utilities import linspace
 
 
 class BinaryOccupancyGrid():
-    """The class is responsible of creating an object
-    representing a discrete map of the environment"""
+    """Representing a discretized 2D map keeping track of all
+    obstacles, pedestrians """
 
-    def __init__(self, map_height, map_width, map_resolution, box_size: float,
+    def __init__(self, map_height: float, map_width: float,
+                 map_resolution: float, box_size: float,
                  get_obstacle_coords: Callable[[], np.ndarray],
                  get_pedestrian_coords: Callable[[], np.ndarray]):
-        # If no arguments passed create default map.
-        # Use this if you plan to load an existing map from file
-        """map_height: height of the map in meters
-           map_length: lenght of the map in meters
-           map_resolution: number of cells per meters
-           grid_size: number of cells along x and y axis
-           cell_size: dimension of a single cell expressed in meters"""
-
+        self.map_height = map_height
+        self.map_width = map_width
+        self.map_resolution = map_resolution
         self.box_size = box_size
         self.get_obstacle_coords = get_obstacle_coords
         self.get_pedestrian_coords = get_pedestrian_coords
 
-        self.map_height = map_height
-        self.map_width = map_width
-        self.map_resolution = map_resolution
-        self.grid_size = dict()
-        self.grid_size['x'] = int(np.ceil(self.map_width * self.map_resolution))
-        self.grid_size['y'] = int(np.ceil(self.map_height * self.map_resolution))
+        self.grid_width = int(np.ceil(self.map_width * self.map_resolution))
+        self.grid_height = int(np.ceil(self.map_height * self.map_resolution))
 
-        self.cell_size = dict()
-        self.cell_size['x'] = self.map_width/(self.grid_size['x'])
-        self.cell_size['y'] = self.map_height/(self.grid_size['y'])
-        x = linspace(0 + self.cell_size['x'] / 2, self.map_width - self.cell_size['x'] / 2, self.grid_size['x'])
-        y = linspace(0 + self.cell_size['y'] / 2, self.map_height - self.cell_size['y'] / 2, self.grid_size['y'])
-        y = np.flip(y)
-        self.x, self.y = np.meshgrid(x, y)
-        self.min_val = np.array([self.x[0,  0] - self.cell_size['x'] / 2, self.y[-1, 0] - self.cell_size['y'] / 2])
-        self.max_val = np.array([self.x[0, -1] + self.cell_size['x'] / 2, self.y[ 0, 0] + self.cell_size['y'] / 2])
-
-        # TODO: refactor the logic to use (x, y) coordinates instead of (y, x) coordinates
-        self.occupancy_moving_objects = np.zeros((self.grid_size['y'], self.grid_size['x']), dtype = bool)
-        self.occupancy_static_objects = self.occupancy_moving_objects.copy()
-        self.occupancy_robot = self.occupancy_moving_objects.copy()
-        self.occupancy_overall = self.occupancy_moving_objects.copy()
-
-        self.grid_origin = [0, 0]
-        self.add_noise = True
-        self.move_map_frame([self.map_width / 2, self.map_height / 2])
-        self.initialize_static_objects()
+        self.occupancy_static_objects, self.obstacle_coordinates = \
+            self._initialize_static_objects()
+        occ_shape = (self.grid_width, self.grid_height)
+        self.occupancy_moving_objects = np.zeros(occ_shape, dtype=bool)
         self.update_moving_objects()
+
+    @property
+    def occupancy_overall(self) -> np.ndarray:
+        return np.logical_or(self.occupancy_moving_objects, self.occupancy_static_objects)
 
     @property
     def occupancy_overall_xy(self) -> np.ndarray:
         # info: swap (y, x) coordinates back to (x, y) coordinates
+        # TODO: remove this dirty hack once the class is refactored
         return self.occupancy_overall[:, ::-1]
 
-    def get_robot_occupancy(self, robot_pos: Vec2D, coll_distance: float) -> np.ndarray:
+    def is_collision(self, robot_pos: Vec2D, collision_distance: float) -> bool:
+        return self.is_pedestrians_collision(robot_pos, collision_distance) \
+            or self.is_obstacle_collision(robot_pos, collision_distance)
+
+    def is_obstacle_collision(self, robot_pos: Vec2D, collision_distance: float) -> bool:
+        occupancy = self._get_robot_occupancy(robot_pos, collision_distance)
+        return np.logical_and(self.occupancy_static_objects, occupancy).any()
+
+    def is_pedestrians_collision(self, robot_pos: Vec2D, collision_distance: float) -> bool:
+        occupancy = self._get_robot_occupancy(robot_pos, collision_distance)
+        return np.logical_and(self.occupancy_moving_objects, occupancy).any()
+
+    def position_bounds(self, margin: float) -> Tuple[List[float], List[float]]:
+        # TODO: figure out what "margin" is supposed to achieve
+        low_bound  = [-self.box_size, -self.box_size, -np.pi]
+        high_bound = [self.box_size, self.box_size,  np.pi]
+        return low_bound, high_bound
+
+    def update_moving_objects(self, map_margin: float=1.5):
+        self.occupancy_moving_objects = self._compute_moving_objects_occupancy()
+
+    def _world_coords_to_grid_cell(self, x: float, y: float) -> Tuple[int, int]:
+        """Map coordinates from a continuous 2D world to a discrete 2D grid.
+          x: [-box_x, box_x] -> [0, grid_width  - 1],
+          y: [-box_y, box_y] -> [0, grid_height - 1]"""
+        scaled_x = (x + self.box_size) / (2 * self.box_size) * self.grid_width
+        scaled_y = (y + self.box_size) / (2 * self.box_size) * self.grid_height
+        grid_x = min(floor(scaled_x), self.grid_width - 1)
+        grid_y = min(floor(scaled_y), self.grid_height - 1)
+        # info: min() function handles the rare case of an index overflow
+        #       when processing x = self.grid_width and/or y = self.grid_height
+        return grid_x, grid_y
+
+    def _grid_cell_centroid_to_world_coords(self, x: int, y: int) -> Tuple[float, float]:
+        """Map coordinates from a continuous 2D world to a discrete 2D grid.
+          x: [0, grid_width  - 1] -> [-box_x, box_x],
+          y: [0, grid_height - 1] -> [-box_y, box_y]"""
+        world_x = (x + 0.5) / self.grid_width * 2 * self.box_size - self.box_size
+        world_y = (y + 0.5) / self.grid_height * 2 * self.box_size - self.box_size
+        return world_x, world_y
+
+    def _is_in_bounds(self, x: float, y: float) -> bool:
+        # TODO: add a more sophisticated approach to this
+        return -self.box_size <= x < self.box_size \
+            and -self.box_size <= y < self.box_size
+
+    def _get_robot_occupancy(self, robot_pos: Vec2D, coll_distance: float) -> np.ndarray:
         rob_matrix = np.zeros(self.occupancy_moving_objects.shape, dtype=bool)
         idx = self.convert_world_to_grid_no_error(np.array(robot_pos.as_list)[np.newaxis, :])
         int_radius_step = round(self.map_resolution * coll_distance)
         return fill_surrounding(rob_matrix, int_radius_step, idx)
 
-    def position_bounds(self, margin: float):
-        # TODO: figure out what this does
+    def _initialize_static_objects(self) -> Tuple[np.ndarray, np.ndarray]:
+        occ_shape = (self.grid_width, self.grid_height)
+        occupancy = np.zeros(occ_shape, dtype=bool)
 
-        x_idx_min = round(margin * self.grid_size['x'])
-        x_idx_max = round((1 - margin) * self.grid_size['x'])
+        all_coords = self.get_obstacle_coords()
+        coords_in_bounds = [pos for pos in all_coords \
+                            if pos.size != 0 and self._is_in_bounds(pos[0], pos[1])]
+        grid_coords = [self._world_coords_to_grid_cell(pos[0], pos[1])
+                       for pos in coords_in_bounds]
 
-        y_idx_min = round(margin * self.grid_size['y'])
-        y_idx_max = round((1 - margin) * self.grid_size['y'])
-
-        low_bound  = [self.x[0, x_idx_min], self.y[y_idx_min, 0], -np.pi]
-        high_bound = [self.x[0, x_idx_max], self.y[y_idx_max, 0],  np.pi]
-        return low_bound, high_bound
-
-    def is_collision(self, robot_pos: Vec2D, collision_distance: float):
-        return self.is_pedestrians_collision(robot_pos, collision_distance) \
-            or self.is_obstacle_collision(robot_pos, collision_distance)
-
-    def is_obstacle_collision(self, robot_pos: Vec2D, collision_distance: float) -> bool:
-        occupancy = self.get_robot_occupancy(robot_pos, collision_distance)
-        return np.logical_and(self.occupancy_static_objects, occupancy).any()
-
-    def is_pedestrians_collision(self, robot_pos: Vec2D, collision_distance: float) -> bool:
-        occupancy = self.get_robot_occupancy(robot_pos, collision_distance)
-        return np.logical_and(self.occupancy_moving_objects, occupancy).any()
-
-    def set_fixed_objects_occupancy(self, pair, val):
-        # pair must be a (m,2) list or numpy array
-        pair = np.array(pair)
-        val = np.array(val, dtype=bool)
-
-        # need to search in world coordinates!
-        # first check if input is valid
-        pair = self.check_if_valid_world_coordinates(pair)
-        if not pair.any():
-            return
-
-        # now if pair is valid start searching the indexes of the corrisponding x and y values
-        idx = self.world_coords_to_grid_index(pair)
-        for i in range(idx.shape[0]):
-            self.occupancy_static_objects[idx[i, 0], idx[i, 1]] = val[i]
-
-        self.obstacle_coordinates = np.concatenate([
-            self.x[self.occupancy_static_objects][:, np.newaxis],
-            self.y[self.occupancy_static_objects][:, np.newaxis]], axis=1)
+        for x, y in grid_coords:
+            occupancy[x, y] = 1
 
         # make the map boundary an "obstacle"
-        self.occupancy_static_objects[:, 0] = True
-        self.occupancy_static_objects[:, -1] = True
-        self.occupancy_static_objects[0, :] = True
-        self.occupancy_static_objects[-1, :] = True
-        self.occupancy_overall = self.occupancy_static_objects
+        occupancy[:, 0] = 1
+        occupancy[:, -1] = 1
+        occupancy[0, :] = 1
+        occupancy[-1, :] = 1
 
-    def set_moving_objects_occupancy(self, pair, val):
-        # pair must be a (m,2) list or numpy array
-        pair = np.array(pair)
-        val = np.array(val, dtype=bool)
-        val = val if val.shape else val * np.ones((pair.shape[0], 1))
-
-        # need to search in world coordinates!
-        # first check if input is valid
-        pair = self.check_if_valid_world_coordinates(pair)
-        if not pair.any():
-            return None
-
-        # now if pair is valid start searching the indexes of the corrisponding x and y values
-        idx = self.world_coords_to_grid_index(pair)
-        for i in range(idx.shape[0]):
-            self.occupancy_moving_objects[idx[i, 0], idx[i, 1]] = val[i]
-        self.update_overall_occupancy()
-
-    def update_overall_occupancy(self):
-        self.occupancy_overall = np.logical_or(
-            self.occupancy_moving_objects, self.occupancy_static_objects)
-
-    def inflate(self, radius: float, fixed_objects_map: bool=False):
-        """Fill the occupancy around an object in circle shape"""
-        # create a copy of the occupancy matrix
+        radius = 0.3
         int_radius_step = round(self.map_resolution * radius)
-        if fixed_objects_map:  # problem here!!!!
-            eval_points = np.asarray(np.where(self.occupancy_static_objects)).T
-            self.occupancy_static_objects = fill_surrounding(
-                self.occupancy_static_objects, int_radius_step, eval_points)
-            self.update_overall_occupancy()
-        else:
-            eval_points = np.asarray(np.where(self.occupancy_moving_objects)).T
-            self.occupancy_moving_objects = fill_surrounding(
-                self.occupancy_moving_objects, int_radius_step, eval_points, add_noise=self.add_noise)
-            self.update_overall_occupancy()
+        eval_points = np.asarray(np.where(occupancy))
+        occupancy = fill_surrounding(occupancy, int_radius_step, eval_points)
+
+        return occupancy, np.array(coords_in_bounds)
+
+    def _compute_moving_objects_occupancy(self) -> np.ndarray:
+        occ_shape = (self.grid_width, self.grid_height)
+        occupancy = np.zeros(occ_shape, dtype=bool)
+
+        peds_pos = self.get_pedestrian_coords()
+        grid_coords = [
+            self._world_coords_to_grid_cell(pos[0], pos[1])
+            for pos in peds_pos
+            if pos.size != 0 and self._is_in_bounds(pos[0], pos[1])
+        ]
+
+        for x, y in grid_coords:
+            occupancy[x, y] = 1
+
+        radius= 0.4
+        int_radius_step = round(self.map_resolution * radius)
+        eval_points = np.asarray(np.where(occupancy))
+        occupancy = fill_surrounding(occupancy, int_radius_step, eval_points, add_noise=True)
+
+        return occupancy
 
     def check_if_valid_world_coordinates(self, pair, margin=0):
         if isinstance(pair, list):
@@ -174,8 +159,9 @@ class BinaryOccupancyGrid():
 
     def check_if_valid_grid_index(self,pair):
         for i in range(pair.shape[0]):
-            if pair[i,0] < 0 or pair[i,0] > self.grid_size['y'] or pair[i,1] < 0 or pair[i,1] > self.grid_size['x'] or not \
-                issubclass(pair.dtype.type,np.integer):
+            if pair[i,0] < 0 or pair[i,0] > self.grid_size['y'] or pair[i,1] < 0 \
+                    or pair[i,1] > self.grid_size['x'] or not \
+                    issubclass(pair.dtype.type,np.integer):
                 return False
         return True
 
@@ -193,53 +179,10 @@ class BinaryOccupancyGrid():
         return val
 
     def convert_world_to_grid_no_error(self, pair):
+        # TODO: this is 100% BS, refactor!!!
         return np.concatenate(
             (np.abs(self.y[:, 0][:, np.newaxis] - pair[:, 1].T).argmin(axis=0)[:, np.newaxis],
              np.abs(self.x[0, :][:, np.newaxis] - pair[:, 0].T).argmin(axis=0)[:, np.newaxis]), axis=1)
-
-    def initialize_static_objects(self, map_margin=1.5):
-        if self.x[0, 0] == self.cell_size['x'] / 2:
-            maps_alignment_offset = map_margin * self.box_size
-        else:
-            maps_alignment_offset = 0
-
-        # assign fixed obstacles to map
-        tmp = [item for item in self.get_obstacle_coords() if item.size != 0]
-        obs_coordinates = np.concatenate(tmp)
-        new_obs_coordinates = obs_coordinates + np.ones((obs_coordinates.shape[0], 2)) * maps_alignment_offset
-        # reset occupancy map
-        self.occupancy_static_objects = np.zeros(self.occupancy_static_objects.shape, dtype=bool)
-        self.set_fixed_objects_occupancy(new_obs_coordinates, np.ones((obs_coordinates.shape[0], 1), dtype=bool))
-        self.inflate(radius=.3, fixed_objects_map=True)
-
-    #update map from pedestrians simulation environment
-    def update_moving_objects(self, map_margin: float=1.5):
-        # shift peds sim map to top-left quadrant if necessary
-        if self.x[0, 0] == self.cell_size['x'] / 2:
-            maps_alignment_offset = map_margin * self.box_size
-        else:
-            maps_alignment_offset = 0
-
-        # get peds states
-        peds_pos = self.get_pedestrian_coords()
-        # add offset to pedestrians maps (it can also have negative coordinates values)
-        n_pedestrians = peds_pos.shape[0]
-        peds_new_coordinates = peds_pos + np.ones((n_pedestrians, 2)) * maps_alignment_offset
-        # reset occupancy map
-        self.occupancy_moving_objects = np.zeros(self.occupancy_moving_objects.shape,dtype = bool)
-        # assign pedestrians positions to robotMap
-        self.set_moving_objects_occupancy(peds_new_coordinates, np.ones((n_pedestrians,1)))
-        self.inflate(radius=.4) # inflate pedestrians only
-
-    def move_map_frame(self, new_position):
-        ''' This method will change the position of the grid 
-            origin. By default when constructing '''
-        self.x = self.x - new_position[0]
-        self.y = self.y - new_position[1]
-        self.grid_origin = [new_position[0], new_position[1]]
-
-        self.min_val = np.array([self.x[0,  0] - self.cell_size['x'] / 2, self.y[-1, 0] - self.cell_size['y'] / 2])
-        self.max_val = np.array([self.x[0, -1] + self.cell_size['x'] / 2, self.y[ 0, 0] + self.cell_size['y'] / 2])
 
 
 def fill_surrounding(matrix, int_radius_step, coords: np.ndarray, add_noise = False):
@@ -266,7 +209,8 @@ def fill_surrounding(matrix, int_radius_step, coords: np.ndarray, add_noise = Fa
             matrix = n_closest_fill(matrix, coords[i, :], int_radius_step, bool_subm_i) 
         else:
             matrix_filled = fill_surrounding_brute(matrix.copy(),int_radius_step,coords[i,0],coords[i,1])
-            matrix = np.logical_or(matrix,matrix_filled)
+            # TODO: don't overwrite the input array, make this work in-place to avoid allocation
+            matrix = np.logical_or(matrix, matrix_filled)
 
     return matrix
 

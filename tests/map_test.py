04e08134a5270a9b37536be5c7bd09af037f6535
Marco Tröster <marco@troester-gmbh.de>
2022-11-01 22:40:52 +0100

    add basic tests for detecting collisions

--- tests/map_test.py
+++ tests/map_test.py
@@ -1,5 +1,6 @@
 import numpy as np
 from robot_sf.map import BinaryOccupancyGrid
+from robot_sf.vector import Vec2D
 
 
 def test_create_map():
@@ -28,5 +29,15 @@ def test_is_out_of_bounds():
     assert not _map.is_in_bounds(10, -10.0000001)
 
 
-def test_is_collision():
-    pass
+def test_is_collision_with_obstacle():
+    obstacle_pos = np.random.uniform(-10, 10, size=(2))
+    robot_pos = Vec2D(obstacle_pos[0], obstacle_pos[1])
+    _map = BinaryOccupancyGrid(40, 40, 10, 10, lambda: [obstacle_pos], lambda: np.array([]))
+    assert _map.is_obstacle_collision(robot_pos, 2)
+
+
+def test_is_collision_with_pedestrian():
+    ped_pos = np.random.uniform(-10, 10, size=(2))
+    robot_pos = Vec2D(ped_pos[0], ped_pos[1])
+    _map = BinaryOccupancyGrid(40, 40, 10, 10, lambda: np.array([]), lambda: np.array([ped_pos]))
+    assert _map.is_pedestrians_collision(robot_pos, 2)

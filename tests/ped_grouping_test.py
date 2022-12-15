46c7547740e1f62e546cefcb79dbaf0bec953800
Marco Tröster <marco@troester-gmbh.de>
2022-12-15 15:09:07 +0100

    add pedestrian grouping test stubs

---
+++ tests/ped_grouping_test.py
@@ -0,0 +1,29 @@
+
+
+
+def test_join_single_pedestrian_to_group():
+    pass
+
+
+def test_split_single_pedestrian_from_group():
+    pass
+
+
+def test_join_group_to_group():
+    pass
+
+
+def test_split_group_from_group():
+    pass
+
+
+def test_stop_group():
+    pass
+
+
+def test_unlock_group():
+    pass
+
+
+def test_change_goal_of_group():
+    pass

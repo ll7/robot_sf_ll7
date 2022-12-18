8d1c4e9dcd262f4d20e1fae3297302a0dd5b635c
Marco Tröster <marco@troester-gmbh.de>
2022-12-18 02:02:43 +0100

    add little benchmark as simulator test

--- tests/env_test.py
+++ tests/env_test.py
@@ -1,6 +1,17 @@
 from robot_sf.robot_env import RobotEnv
 
 
-def test_sample():
+def test_can_create_env():
     env = RobotEnv()
     assert env is not None
+
+
+def test_can_simulate_with_pedestrians():
+    total_steps = 1000
+    env = RobotEnv(difficulty=2)
+    env.reset()
+    for _ in range(total_steps):
+        rand_action = env.action_space.sample()
+        _, _, done, _ = env.step(rand_action)
+        if done:
+            env.reset()

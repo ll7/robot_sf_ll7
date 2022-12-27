a2945e6089ced4655f883923110d04e46b429276
Marco Tröster <marco@troester-gmbh.de>
2022-12-27 18:05:02 +0100

    enable tensorboard logs for training monitoring

--- scripts/training.py
+++ scripts/training.py
@@ -1,13 +1,13 @@
-from stable_baselines3 import A2C
+from stable_baselines3.a2c.a2c import A2C
 from stable_baselines3.common.env_util import make_vec_env
 from robot_sf.robot_env import RobotEnv
 
 
 def training():
-    env = make_vec_env(lambda: RobotEnv(difficulty=2), n_envs=4)
-    model = A2C("MlpPolicy", env)
-    model.learn(total_timesteps=int(2e6), progress_bar=True)
-    model.save("./model/dqn_model")
+    env = make_vec_env(lambda: RobotEnv(difficulty=2), n_envs=50)
+    model = A2C("MlpPolicy", env, tensorboard_log="./logs/a2c_logs/")
+    model.learn(total_timesteps=50_000_000, progress_bar=True)
+    model.save("./model/a2c_model")
 
 
 if __name__ == '__main__':

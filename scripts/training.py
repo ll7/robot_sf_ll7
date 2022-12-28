4a9e848510a18538c3358457e4f4b48079c16c83
Marco Tröster <marco@troester-gmbh.de>
2022-12-28 18:35:01 +0100

    make use of cpu cores during training

--- scripts/training.py
+++ scripts/training.py
@@ -1,10 +1,11 @@
 from stable_baselines3.a2c.a2c import A2C
 from stable_baselines3.common.env_util import make_vec_env
+from stable_baselines3.common.vec_env import SubprocVecEnv
 from robot_sf.robot_env import RobotEnv
 
 
 def training():
-    env = make_vec_env(lambda: RobotEnv(difficulty=2), n_envs=50)
+    env = make_vec_env(lambda: RobotEnv(difficulty=2), n_envs=50, vec_env_cls=SubprocVecEnv)
     model = A2C("MlpPolicy", env, tensorboard_log="./logs/a2c_logs/")
     model.learn(total_timesteps=50_000_000, progress_bar=True)
     model.save("./model/a2c_model")

64550e0a1581f6ea78364e432024f9a24146762e
Marco Tröster <marco@troester-gmbh.de>
2023-01-18 23:46:08 +0100

    fix map unit tests (use more precise attributes)

--- tests/map_test.py
+++ tests/map_test.py
@@ -33,7 +33,7 @@ def test_is_collision_with_obstacle_segment_fully_contained_inside_circle():
     obstacle_pos = np.random.uniform(-10, 10, size=(1, 4))
     robot_pos = (obstacle_pos[0, 0], obstacle_pos[0, 1])
     _map = ContinuousOccupancy(10, 10, lambda: robot_pos, lambda: None, lambda: obstacle_pos, lambda: np.array([[20, 20]]), robot_radius=2)
-    assert _map.is_robot_collision
+    assert _map.is_obstacle_collision
 
 
 def test_is_collision_with_obstacle_segment_outside_circle():
@@ -42,11 +42,11 @@ def test_is_collision_with_obstacle_segment_outside_circle():
     robot_pos = (middle[0], middle[1])
     radius = dist(obstacle_pos[0, :2], obstacle_pos[0, 2:]) / 2.1
     _map = ContinuousOccupancy(10, 10, lambda: robot_pos, lambda: None, lambda: obstacle_pos, lambda: np.array([[20, 20]]), radius)
-    assert _map.is_robot_collision
+    assert _map.is_obstacle_collision
 
 
 def test_is_collision_with_pedestrian():
     ped_pos = np.random.uniform(-10, 10, size=(2))
     robot_pos = (ped_pos[0], ped_pos[1])
     _map = ContinuousOccupancy(40, 40, lambda: robot_pos, lambda: None, lambda: np.array([[]]), lambda: np.array([ped_pos]), robot_radius=2)
-    assert _map.is_robot_collision
+    assert _map.is_pedestrian_collision

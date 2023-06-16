bc4e50232c4c940dd11a78cc6b69ad965c97a048
Marco Tröster <marco@troester-gmbh.de>
2023-06-16 19:04:39 +0200

    fix feature extractor shapes

--- robot_sf/feature_extractor.py
+++ robot_sf/feature_extractor.py
@@ -9,10 +9,11 @@ from robot_sf.robot_env import OBS_DRIVE_STATE, OBS_RAYS
 
 
 class DynamicsExtractor(BaseFeaturesExtractor):
-    def __init__(self, observation_space: spaces.Dict, ray_features: int=256):
+    def __init__(self, observation_space: spaces.Dict):
         rays_space: spaces.Box = observation_space.spaces[OBS_RAYS]
         drive_state_space: spaces.Box = observation_space.spaces[OBS_DRIVE_STATE]
         drive_state_features = np.prod(drive_state_space.shape)
+        ray_features = 16 * (rays_space.shape[1] // 16)
         total_features = ray_features + drive_state_features
         super().__init__(observation_space, features_dim=total_features)
 

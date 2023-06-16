d0817f11cf5af1f407bf42dbd443b60355e52074
Marco Tröster <marco@troester-gmbh.de>
2023-06-16 18:57:53 +0200

    rework feature extractor - use smaller kernels in conv layers - reduce output filters to acutally downsample data - remove outgoing dense layer - keep drive state as-is, just flatten

--- robot_sf/feature_extractor.py
+++ robot_sf/feature_extractor.py
@@ -9,38 +9,29 @@ from robot_sf.robot_env import OBS_DRIVE_STATE, OBS_RAYS
 
 
 class DynamicsExtractor(BaseFeaturesExtractor):
-    def __init__(
-            self, observation_space: spaces.Dict,
-            ray_features: int = 256,
-            drive_state_features: int = 64):
-        super().__init__(observation_space, features_dim=ray_features+drive_state_features)
-
+    def __init__(self, observation_space: spaces.Dict, ray_features: int=256):
         rays_space: spaces.Box = observation_space.spaces[OBS_RAYS]
         drive_state_space: spaces.Box = observation_space.spaces[OBS_DRIVE_STATE]
+        drive_state_features = np.prod(drive_state_space.shape)
+        total_features = ray_features + drive_state_features
+        super().__init__(observation_space, features_dim=total_features)
 
         self.ray_extractor = nn.Sequential(
-            nn.Conv1d(rays_space.shape[0], 64, kernel_size=9, stride=2, padding=4),
+            nn.Conv1d(rays_space.shape[0], 64, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Conv1d(64, 64, kernel_size=9, stride=2, padding=4),
+            nn.Conv1d(64, 16, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Conv1d(64, 64, kernel_size=9, stride=2, padding=4),
+            nn.Conv1d(16, 16, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Conv1d(64, 64, kernel_size=9, stride=2, padding=4),
+            nn.Conv1d(16, 16, kernel_size=3, stride=2, padding=1),
             nn.ReLU(),
             nn.Dropout(0.3),
-            nn.Flatten(),
-            nn.Linear((rays_space.shape[1] // 16) * 64, ray_features))
+            nn.Flatten())
 
-        self.drive_state_extractor = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(np.prod(drive_state_space.shape), 64),
-            nn.Linear(64, drive_state_features))
-
-        self.extractors = nn.ModuleDict(
-            { OBS_RAYS: self.ray_extractor, OBS_DRIVE_STATE: self.drive_state_extractor })
+        self.drive_state_extractor = nn.Sequential(nn.Flatten())
 
     def forward(self, obs: dict) -> th.Tensor:
         ray_x = self.ray_extractor(obs[OBS_RAYS])

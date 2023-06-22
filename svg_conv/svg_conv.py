91d3399f1e4852945dd6f259dcacbc61861fc843
Marco Tröster <marco@troester-gmbh.de>
2023-06-22 21:58:14 +0200

    adjust coordinates to 1:1 scale

--- svg_conv/svg_conv.py
+++ svg_conv/svg_conv.py
@@ -46,12 +46,17 @@ def serialize_mapjson(poly_points: List[List[Vec2D]]) -> str:
     return json.dumps(map_obj)
 
 
+def scale(p: Vec2D, factor: float) -> Vec2D:
+    return p[0] * factor, p[1] * factor
+
+
 def convert_map(input_svg_file: str, output_json_file: str):
     svg = SVG.parse(input_svg_file)
     house_color = (217, 208, 201)
     paths = paths_of_svg(svg)
     paths = filter_paths_by_color(paths, house_color)
     poly_points = points_of_paths(paths)
+    poly_points = [[scale(p, 1.62) for p in poly] for poly in poly_points]
     map_json = serialize_mapjson(poly_points)
     with open(output_json_file, 'w') as file:
         file.write(map_json)

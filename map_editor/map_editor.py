9fb544996a6e6d3d088c1e2f9ffdb899e1cb4436
Marco Tröster <marco@troester-gmbh.de>
2023-06-22 16:11:00 +0200

    support ped routes in map editor

--- map_editor/map_editor.py
+++ map_editor/map_editor.py
@@ -1,108 +1,20 @@
 import os
+from enum import IntEnum
 from time import sleep
-from typing import Tuple, Callable, Union, List
+from dataclasses import dataclass
+from typing import Tuple, Callable, Union, List, Iterable
 from threading import Thread
 
-import turtle
 import tkinter as tk
 import tkinter.scrolledtext as tks
 
 from map_editor.map_file_parser import \
-    parse_mapfile_text, VisualizableMapConfig, format_waypoints_as_json
+    parse_mapfile_text, VisualizableMapConfig
 
 Vec2D = Tuple[float, float]
 Range2D = Tuple[float, float] # (low, high)
-MapBounds = Tuple[Range2D, Range2D] # (dx, dy)
+MapBounds = Tuple[Range2D, Range2D] # ((min_x, max_x), (min_y, max_y))
 Rect = Tuple[Vec2D, Vec2D, Vec2D]
-
-
-def draw_line(turtle: turtle.RawTurtle, p1: Vec2D, p2: Vec2D):
-    turtle.up()
-    turtle.setpos(p1)
-    turtle.down()
-    turtle.setpos(p2)
-
-
-def rect_points(rect: Rect) -> List[Vec2D]:
-    def add_vec(v1: Vec2D, v2: Vec2D) -> Vec2D:
-        return v1[0] + v2[0], v1[1] + v2[1]
-    def sub_vec(v1: Vec2D, v2: Vec2D) -> Vec2D:
-        return v1[0] - v2[0], v1[1] - v2[1]
-    p1, p2, p3 = rect
-    p4 = add_vec(sub_vec(p3, p2), p1)
-    return [p1, p2, p3, p4]
-
-
-def draw_rect(turtle: turtle.RawTurtle, points: List[Vec2D]):
-    p1, p2, p3, p4 = points
-    turtle.up()
-    turtle.setpos(p1)
-    turtle.down()
-    turtle.setpos(p2)
-    turtle.setpos(p3)
-    turtle.setpos(p4)
-    turtle.setpos(p1)
-
-
-def draw_circle(turtle: turtle.RawTurtle, center: Vec2D, radius: float):
-    turtle.up()
-    turtle.setpos((center[0], center[1] - radius))
-    turtle.down()
-    turtle.circle(radius, steps=50)
-
-
-class RouteBuilder:
-    def __init__(
-            self, frame: tk.Frame, canvas: tk.Canvas,
-            render_waypoints: Callable[[List[Vec2D]], None],
-            fetch_map_bounds: Callable[[], MapBounds]):
-        self.canvas = canvas
-        self.render_waypoints = render_waypoints
-        self.fetch_map_bounds = fetch_map_bounds
-
-        self.btn_new_route = tk.Button(frame, text="New Route", command=self.new_route)
-        self.btn_undo = tk.Button(frame, text="Undo", command=self.undo_waypoint)
-        self.route_as_text = tk.StringVar(frame, "[]")
-        self.text_route = tk.Entry(frame, textvariable=self.route_as_text)
-        self.text_route.config(state=tk.DISABLED)
-
-        self.mouse_pos = (0, 0)
-        self.last_config: VisualizableMapConfig
-        self.waypoints: List[Vec2D] = []
-
-        def track_mouse_pos(event):
-            self.mouse_pos = (event.x, event.y)
-
-        self.canvas.bind('<Motion>', track_mouse_pos)
-        self.canvas.bind('<Button-1>', lambda e: self.add_waypoint(self.mouse_pos))
-
-    def pack(self):
-        self.btn_undo.pack(side=tk.RIGHT)
-        self.btn_new_route.pack(side=tk.RIGHT)
-        self.text_route.pack(fill="x")
-
-    def new_route(self):
-        self.waypoints = []
-        self._update_ui()
-
-    def add_waypoint(self, p: Vec2D):
-        bounds = self.fetch_map_bounds()
-        if not bounds:
-            print("WARNING: no map bounds specified!")
-            return
-
-        x_margin, y_margin = bounds
-        # TODO: convert canvas mouse position to map coord
-
-    def undo_waypoint(self):
-        if len(self.waypoints) > 0:
-            self.waypoints.pop()
-            self._update_ui()
-
-    def _update_ui(self):
-        new_text = format_waypoints_as_json(self.waypoints)
-        self.route_as_text.set(new_text)
-        self.render_waypoints(self.waypoints)
 
 
 class MapCanvas:
@@ -111,87 +23,174 @@ class MapCanvas:
     ROBOT_GOAL_COLOR = "green"
     ROBOT_SPAWN_COLOR = "blue"
     PED_SPAWN_COLOR = "red"
+    PED_GOAL_COLOR = "darkgreen"
     ROBOT_ROUTE_COLOR = "green"
-    TEMP_ROUTE_COLOR = "orange"
+    PED_CROWDED_COLOR = "orange"
+    PED_ROUTE_COLOR = "yellow"
 
     def __init__(self, frame: tk.Frame):
         self.canvas = tk.Canvas(frame)
         self.canvas.config(width=800, height=800)
-        screen = turtle.TurtleScreen(self.canvas)
-        screen.bgcolor(MapCanvas.BG_COLOR)
-        self.turtle = turtle.RawTurtle(screen, visible=False)
-        screen.tracer(0)
-        self.temp_waypoints = []
 
     def pack(self):
         self.canvas.pack()
 
-    def render_temp_waypoints(self, waypoints: List[Vec2D]):
-        self.temp_waypoints = waypoints
+    def scaling(self, bounds: MapBounds) -> float:
+        canvas_width, canvas_height = self.canvas.winfo_width(), self.canvas.winfo_height()
+        (min_x, max_x), (min_y, max_y) = bounds
+        map_width, map_height = max_x - min_x, max_y - min_y
+        x_scale, y_scale = canvas_width / map_width, canvas_height / map_height
+        return min(x_scale, y_scale)
 
     def render(self, map_config: VisualizableMapConfig):
-        (min_x, max_x), (min_y, max_y) = map_config.x_margin, map_config.y_margin
-        width, height = max_x- min_x, max_y - min_y
-        if width > height:
-            max_y = min_y + width
-        else:
-            max_x = min_x + height
-        self.turtle.screen.setworldcoordinates(min_x, min_y, max_x, max_y)
-        self.turtle.clear()
+        self.canvas.delete("all")
+        scaling = self.scaling((map_config.x_margin, map_config.y_margin))
 
-        self.turtle.color(MapCanvas.OBSTACLE_COLOR)
+        def rect_points(rect: Rect) -> List[Vec2D]:
+            def add_vec(v1: Vec2D, v2: Vec2D) -> Vec2D:
+                return v1[0] + v2[0], v1[1] + v2[1]
+            def sub_vec(v1: Vec2D, v2: Vec2D) -> Vec2D:
+                return v1[0] - v2[0], v1[1] - v2[1]
+            p1, p2, p3 = rect
+            p4 = add_vec(sub_vec(p3, p2), p1)
+            return [p1, p2, p3, p4]
+
+        def scale(p: Vec2D) -> Vec2D:
+            (min_x, _), (min_y, _) = map_config.x_margin, map_config.y_margin
+            return (p[0] - min_x) * scaling, (p[1] - min_y) * scaling
+
+        def draw_rect(canvas: tk.Canvas, rect: List[Vec2D], color="black"):
+            p1, p2, p3, p4 = rect
+            canvas.create_line(scale(p1), scale(p2), fill=color)
+            canvas.create_line(scale(p2), scale(p3), fill=color)
+            canvas.create_line(scale(p3), scale(p4), fill=color)
+            canvas.create_line(scale(p4), scale(p1), fill=color)
+
+        def draw_circle(p: Vec2D, r: float, color="black", fill=None):
+            (x, y), r = scale(p), r * scaling
+            self.canvas.create_oval(x-r, y-r, x+r, y+r, outline=color, fill=fill if fill else color)
+
         for s_x, e_x, s_y, e_y in map_config.obstacles:
-            draw_line(self.turtle, (s_x, s_y), (e_x, e_y))
+            if (s_x, s_y) != (e_x, e_y):
+                self.canvas.create_line(scale((s_x, s_y)), scale((e_x, e_y)), fill=MapCanvas.OBSTACLE_COLOR)
 
-        self.turtle.color(MapCanvas.ROBOT_GOAL_COLOR)
-        for rect in map_config.goal_zones:
-            draw_rect(self.turtle, rect_points(rect))
-
-        self.turtle.color(MapCanvas.ROBOT_SPAWN_COLOR)
         for rect in map_config.robot_spawn_zones:
-            draw_rect(self.turtle, rect_points(rect))
+            draw_rect(self.canvas, rect_points(rect), MapCanvas.ROBOT_SPAWN_COLOR)
 
-        self.turtle.color(MapCanvas.PED_SPAWN_COLOR)
+        for rect in map_config.robot_goal_zones:
+            draw_rect(self.canvas, rect_points(rect), MapCanvas.ROBOT_GOAL_COLOR)
+
         for rect in map_config.ped_spawn_zones:
-            draw_rect(self.turtle, rect_points(rect))
+            draw_rect(self.canvas, rect_points(rect), MapCanvas.PED_SPAWN_COLOR)
 
-        self.turtle.color(MapCanvas.ROBOT_ROUTE_COLOR)
-        self.turtle.fillcolor(MapCanvas.ROBOT_ROUTE_COLOR)
+        for rect in map_config.ped_goal_zones:
+            draw_rect(self.canvas, rect_points(rect), MapCanvas.PED_GOAL_COLOR)
+
+        for rect in map_config.ped_crowded_zones:
+            draw_rect(self.canvas, rect_points(rect), MapCanvas.PED_CROWDED_COLOR)
+
         for route in map_config.robot_routes:
             for p in route.waypoints:
-                draw_circle(self.turtle, p, radius=1)
+                draw_circle(p, 1, MapCanvas.ROBOT_ROUTE_COLOR)
 
-        self.turtle.color(MapCanvas.TEMP_ROUTE_COLOR)
-        self.turtle.fillcolor(MapCanvas.TEMP_ROUTE_COLOR)
-        for p in self.temp_waypoints:
-            draw_circle(self.turtle, p, radius=1)
+        for route in map_config.ped_routes:
+            for p in route.waypoints:
+                draw_circle(p, 1, MapCanvas.PED_ROUTE_COLOR)
 
-        self.turtle.up()
+
+class MapToolbarMode(IntEnum):
+    NONE            = 0
+    NEW_ROBOT_SPAWN = 1
+    NEW_ROBOT_GOAL  = 2
+    NEW_ROBOT_ROUTE = 3
+    NEW_PED_SPAWN   = 4
+    NEW_PED_GOAL    = 5
+    NEW_PED_ROUTE   = 6
+    NEW_OBSTACLE    = 7
+
+
+class MapEditorToolbar:
+    COLOR_BTN_DEFAULT = "gray"
+    COLOR_BTN_HIGHLIGHTED = "yellow"
+
+    def __init__(self, frame: tk.Frame, on_mode_changed: Callable[[MapToolbarMode], None]):
+        self.on_mode_changed = on_mode_changed
+        self.mode = MapToolbarMode.NONE
+
+        self.buttons_by_mode = { }
+        self.buttons_by_mode[MapToolbarMode.NEW_ROBOT_SPAWN] = tk.Button(
+            frame, text="Robot Spawn", command=lambda: self.change_mode(MapToolbarMode.NEW_ROBOT_SPAWN))
+        self.buttons_by_mode[MapToolbarMode.NEW_ROBOT_GOAL] = tk.Button(
+            frame, text="Robot Goal", command=lambda: self.change_mode(MapToolbarMode.NEW_ROBOT_GOAL))
+        self.buttons_by_mode[MapToolbarMode.NEW_ROBOT_ROUTE] = tk.Button(
+            frame, text="Robot Route", command=lambda: self.change_mode(MapToolbarMode.NEW_ROBOT_ROUTE))
+        self.buttons_by_mode[MapToolbarMode.NEW_PED_SPAWN] = tk.Button(
+            frame, text="Ped Spawn", command=lambda: self.change_mode(MapToolbarMode.NEW_PED_SPAWN))
+        self.buttons_by_mode[MapToolbarMode.NEW_PED_GOAL] = tk.Button(
+            frame, text="Ped Goal", command=lambda: self.change_mode(MapToolbarMode.NEW_PED_GOAL))
+        self.buttons_by_mode[MapToolbarMode.NEW_PED_ROUTE] = tk.Button(
+            frame, text="Ped Route", command=lambda: self.change_mode(MapToolbarMode.NEW_PED_ROUTE))
+
+    @property
+    def buttons(self) -> Iterable[tk.Button]:
+        return self.buttons_by_mode.values()
+
+    def pack(self):
+        for btn in self.buttons:
+            btn.pack(side=tk.TOP, fill="x")
+            btn.configure(background=MapEditorToolbar.COLOR_BTN_DEFAULT)
+
+    def change_mode(self, new_mode: MapToolbarMode):
+        if self.mode == new_mode:
+            self.mode = MapToolbarMode.NONE
+        else:
+            self.mode = new_mode
+
+        for (mode, btn) in self.buttons_by_mode.items():
+            highlight = mode == self.mode
+            new_bg = MapEditorToolbar.COLOR_BTN_HIGHLIGHTED if highlight \
+                else MapEditorToolbar.COLOR_BTN_DEFAULT
+            btn.configure(background=new_bg)
+
+        self.on_mode_changed(self.mode)
 
 
 class TextEditor:
-    def __init__(self, frame: tk.Frame):
+    # TODO: support undo / redo logic with ctrl+z / ctrl+y
+
+    def __init__(self, frame: tk.Frame, clipboard: Callable[[], str]):
         self.input = tks.ScrolledText(frame)
-        self.input.bind("<Control-Key-a>", self.select_all)
+        self.input.bind("<Control-Key-a>", lambda e: self.select_all())
+        # self.input.bind("<Control-Key-v>", lambda e: self.insert_text(clipboard()))
 
     @property
     def text(self) -> str:
-        return self.input.get("1.0", 'end-1c')
+        return self.input.get("1.0", "end-1c")
+
+    @property
+    def has_selected_text(self) -> bool:
+        return not self.input.selection_get()
 
     def pack(self):
         self.input.pack(side=tk.LEFT, fill="both")
 
     def clear_text(self):
-        self.input.delete('1.0', tk.END)
+        self.input.delete("1.0", tk.END)
 
     def append_text(self, text: str):
         self.input.insert(tk.END, text)
 
-    def select_all(self, e: tk.Event):
+    def select_all(self):
         self.input.tag_add(tk.SEL, "1.0", tk.END)
         self.input.mark_set(tk.INSERT, "1.0")
         self.input.see(tk.INSERT)
-        return 'break'
+        return "break"
+
+    # def insert_text(self, text: str):
+    #     if self.has_selected_text:
+    #         self.input.delete(tk.SEL_FIRST, tk.SEL_LAST)
+    #     self.input.insert(tk.CURRENT, text)
+    #     return "break"
 
 
 class MapEditor:
@@ -203,21 +202,16 @@ class MapEditor:
         self.master.protocol("WM_DELETE_WINDOW", self.on_closing)
 
         self.frame_editor = tk.Frame(self.master)
+        self.frame_toolbar = tk.Frame(self.master)
         self.frame_canvas = tk.Frame(self.master)
-        self.frame_route_builder = tk.Frame(self.master)
 
         self.map_canvas = MapCanvas(self.frame_canvas)
-        self.text_editor = TextEditor(self.frame_editor)
-        self.route_builder = RouteBuilder(
-            self.frame_route_builder,
-            self.map_canvas.canvas,
-            self.map_canvas.render_temp_waypoints,
-            lambda: (self.last_config.x_margin, self.last_config.y_margin) \
-                if self.last_config else ((0, 1), (0, 1)))
+        self.text_editor = TextEditor(self.frame_editor, self.master.clipboard_get)
+        self.map_toolbar = MapEditorToolbar(self.frame_toolbar, lambda mode: None)
 
         self.is_shutdown_requested = False
         self.map_rendering_thread: Union[Thread, None] = None
-        self.last_text = ''
+        self.last_text = ""
         self.last_config: Union[VisualizableMapConfig, None] = None
         self._load_example_map()
 
@@ -226,17 +220,15 @@ class MapEditor:
 
         def reload_map():
             config_content = self.text_editor.text
-            if config_content != self.last_text:
-                map_config = parse_mapfile_text(config_content)
-                if map_config:
-                    self.last_config = map_config
-                    self.last_text = config_content
-                    try:
-                        self.map_canvas.render(map_config)
-                    except:
-                        print('data error, cannot display')
-                else:
-                    print('parsing config file failed!')
+            map_config = parse_mapfile_text(config_content) \
+                if config_content != self.last_text else None
+            self.last_config = map_config if map_config else self.last_config
+            self.last_text = config_content if map_config else self.last_text
+            if map_config:
+                try:
+                    self.map_canvas.render(map_config)
+                except:
+                    print("unable to draw map")
 
         def reload_map_as_daemon(frequency_hz: float, is_term: Callable[[], bool]):
             reload_intercal_secs = 1 / frequency_hz
@@ -244,17 +236,19 @@ class MapEditor:
                 reload_map()
                 sleep(reload_intercal_secs)
 
-        RELOAD_FREQUENCY = 4
+        RELOAD_FREQUENCY = 5
         args = (RELOAD_FREQUENCY, lambda: self.is_shutdown_requested)
         self.map_rendering_thread = Thread(target=reload_map_as_daemon, args=args)
         self.map_rendering_thread.start()
         self.master.mainloop()
 
     def pack(self):
+        self.frame_toolbar.pack(side=tk.RIGHT, fill="y")
         self.frame_canvas.pack(side=tk.RIGHT)
-        self.frame_editor.pack(side=tk.LEFT, fill='both')
+        self.frame_editor.pack(side=tk.LEFT, fill="both")
         self.map_canvas.pack()
         self.text_editor.pack()
+        self.map_toolbar.pack()
 
     def on_closing(self):
         if self.map_rendering_thread:
@@ -264,8 +258,8 @@ class MapEditor:
 
     def _load_example_map(self):
         current_dir = os.path.dirname(__file__)
-        example_filepath = os.path.join(current_dir, 'map_example.json')
-        with open(example_filepath, 'r') as file:
+        example_filepath = os.path.join(current_dir, "map_example.json")
+        with open(example_filepath, "r") as file:
             text = file.read()
         self.text_editor.clear_text()
         self.text_editor.append_text(text)
--- map_editor/map_file_parser.py
+++ map_editor/map_file_parser.py
@@ -22,14 +22,18 @@ class VisualizableMapConfig:
     x_margin: Range2D
     y_margin: Range2D
     obstacles: np.ndarray
-    goal_zones: List[Rect] = field(default_factory=list)
     robot_spawn_zones: List[Rect] = field(default_factory=list)
-    ped_spawn_zones: List[Rect] = field(default_factory=list)
+    robot_goal_zones: List[Rect] = field(default_factory=list)
     robot_routes: List[GlobalRoute] = field(default_factory=list)
+    ped_spawn_zones: List[Rect] = field(default_factory=list)
+    ped_goal_zones: List[Rect] = field(default_factory=list)
+    ped_routes: List[GlobalRoute] = field(default_factory=list)
+    ped_crowded_zones: List[Rect] = field(default_factory=list)
 
 
-MAP_VERSION_V0 = 'v0'
-MAP_VERSION_V1 = 'v1'
+MAP_VERSION_V0 = "v0"
+MAP_VERSION_V1 = "v1"
+MAP_VERSION_V2 = "v2"
 
 
 def determine_mapfile_version(text: str) -> Union[str, None]:
@@ -42,19 +46,23 @@ def determine_mapfile_version(text: str) -> Union[str, None]:
     try:
         map_data: dict = json.loads(text)
 
-        required_toplevel_keys = { 'x_margin', 'y_margin' }
+        required_toplevel_keys = { "x_margin", "y_margin" }
         if not contains_all(set(map_data.keys()), required_toplevel_keys):
             return None
 
-        obsolete_v0_toplevel_keys = { 'Obstacles', 'NumberObstacles', 'Created' }
+        obsolete_v0_toplevel_keys = { "Obstacles", "NumberObstacles", "Created" }
         if any([k for k in obsolete_v0_toplevel_keys if k in map_data]):
             return MAP_VERSION_V0
 
-        required_v1_keys = { 'obstacles', 'ped_spawn_zones', 'robot_spawn_zones', 'goal_zones', 'robot_routes' }
+        required_v1_keys = { "obstacles", "ped_spawn_zones", "robot_spawn_zones", "robot_goal_zones", "robot_routes" }
         if not contains_all(set(map_data.keys()), required_v1_keys):
             return MAP_VERSION_V0
 
-        return MAP_VERSION_V1
+        required_v2_keys = { "ped_routes", "ped_goal_zones", "ped_crowded_zones" }
+        if not contains_all(set(map_data.keys()), required_v2_keys):
+            return MAP_VERSION_V1
+
+        return MAP_VERSION_V2
     except:
         return None
 
@@ -64,8 +72,8 @@ def parse_mapfile_text_v0(text: str) -> Union[VisualizableMapConfig, None]:
         map_data = json.loads(text)
 
         all_lines = list()
-        for obstacle in map_data['obstacles']:
-            vertices: List[Vec2D] = map_data['obstacles'][obstacle]['Vertex']
+        for obstacle in map_data["obstacles"]:
+            vertices: List[Vec2D] = map_data["obstacles"][obstacle]["Vertex"]
             edges = list(zip(vertices[:-1], vertices[1:])) \
                 + [(vertices[-1], vertices[0])]
             for (s_x, s_y), (e_x, e_y) in edges:
@@ -73,10 +81,10 @@ def parse_mapfile_text_v0(text: str) -> Union[VisualizableMapConfig, None]:
                 all_lines.append(line)
         obstacles = np.array(all_lines)
 
-        x_margin = map_data['x_margin']
+        x_margin = map_data["x_margin"]
         x_margin = (x_margin[0], x_margin[1])
 
-        y_margin = map_data['y_margin']
+        y_margin = map_data["y_margin"]
         y_margin = (y_margin[0], y_margin[1])
 
         return VisualizableMapConfig(x_margin, y_margin, obstacles)
@@ -89,7 +97,7 @@ def parse_mapfile_text_v1(text: str) -> Union[VisualizableMapConfig, None]:
         map_data = json.loads(text)
 
         all_lines = list()
-        for vertices in map_data['obstacles']:
+        for vertices in map_data["obstacles"]:
             vertices: List[Vec2D]
             edges = list(zip(vertices[:-1], vertices[1:])) \
                 + [(vertices[-1], vertices[0])]
@@ -98,22 +106,62 @@ def parse_mapfile_text_v1(text: str) -> Union[VisualizableMapConfig, None]:
                 all_lines.append(line)
         obstacles = np.array(all_lines)
 
-        routes = [GlobalRoute(o['spawn_id'], o['goal_id'], o['waypoints'])
-                  for o in map_data['robot_routes']]
+        routes = [GlobalRoute(o["spawn_id"], o["goal_id"], o["waypoints"])
+                  for o in map_data["robot_routes"]]
 
-        x_margin = map_data['x_margin']
+        x_margin = map_data["x_margin"]
         x_margin = (x_margin[0], x_margin[1])
 
-        y_margin = map_data['y_margin']
+        y_margin = map_data["y_margin"]
         y_margin = (y_margin[0], y_margin[1])
 
-        goal_zones = map_data['goal_zones']
-        robot_spawn_zones = map_data['robot_spawn_zones']
-        ped_spawn_zones = map_data['ped_spawn_zones']
+        robot_goal_zones = map_data["goal_zones"]
+        robot_spawn_zones = map_data["robot_spawn_zones"]
+        ped_spawn_zones = map_data["ped_spawn_zones"]
 
         return VisualizableMapConfig(
-            x_margin, y_margin, obstacles, goal_zones,
-            robot_spawn_zones, ped_spawn_zones, routes)
+            x_margin, y_margin, obstacles,
+            robot_spawn_zones, robot_goal_zones, routes,
+            ped_spawn_zones)
+    except:
+        return None
+
+
+def parse_mapfile_text_v2(text: str) -> Union[VisualizableMapConfig, None]:
+    try:
+        map_data = json.loads(text)
+
+        all_lines = list()
+        for vertices in map_data["obstacles"]:
+            vertices: List[Vec2D]
+            edges = list(zip(vertices[:-1], vertices[1:])) \
+                + [(vertices[-1], vertices[0])]
+            for (s_x, s_y), (e_x, e_y) in edges:
+                line = [s_x, e_x, s_y, e_y]
+                all_lines.append(line)
+        obstacles = np.array(all_lines)
+
+        robot_routes = [GlobalRoute(o["spawn_id"], o["goal_id"], o["waypoints"])
+                        for o in map_data["robot_routes"]]
+        ped_routes = [GlobalRoute(o["spawn_id"], o["goal_id"], o["waypoints"])
+                      for o in map_data["ped_routes"]]
+
+        x_margin = map_data["x_margin"]
+        x_margin = (x_margin[0], x_margin[1])
+
+        y_margin = map_data["y_margin"]
+        y_margin = (y_margin[0], y_margin[1])
+
+        robot_spawn_zones = map_data["robot_spawn_zones"]
+        robot_goal_zones = map_data["robot_goal_zones"]
+        ped_spawn_zones = map_data["ped_spawn_zones"]
+        ped_goal_zones = map_data["ped_goal_zones"]
+        ped_crowded_zones = map_data["ped_crowded_zones"]
+
+        return VisualizableMapConfig(
+            x_margin, y_margin, obstacles,
+            robot_spawn_zones, robot_goal_zones, robot_routes,
+            ped_spawn_zones, ped_goal_zones, ped_routes, ped_crowded_zones)
     except:
         return None
 
@@ -121,12 +169,13 @@ def parse_mapfile_text_v1(text: str) -> Union[VisualizableMapConfig, None]:
 parsers_by_version = {
     MAP_VERSION_V0: parse_mapfile_text_v0,
     MAP_VERSION_V1: parse_mapfile_text_v1,
+    MAP_VERSION_V2: parse_mapfile_text_v2,
 }
 
 
 def parse_mapfile_text(text: str) -> Union[VisualizableMapConfig, None]:
     version = determine_mapfile_version(text)
-    print(f'file version is {version}')
+    print(f"file version is {version if version else 'invalid'}")
     return parsers_by_version[version](text) if version else None
 
 

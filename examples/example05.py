dca77df7c3583c730bf8cc7eba511650d44aec09
ll7 <sailingmaster92@gmx.de>
2024-01-31 14:52:37 +0100

    Add example05.py and map03.json

---
+++ examples/example05.py
@@ -0,0 +1,15 @@
+"""
+# Example 05: simulate map03
+"""
+
+import pysocialforce as pysf
+
+map_def = pysf.load_map("./maps/map03.json")
+display = pysf.SimulationView(obstacles=map_def.obstacles, scaling=10)
+render_step = lambda t, s: display.render(pysf.to_visualizable_state(t, s))
+simulator = pysf.Simulator_v2(map_def, on_step=render_step)
+
+display.show()
+for step in range(10_000):
+    simulator.step()
+display.exit()

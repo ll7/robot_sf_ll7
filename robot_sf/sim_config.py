2578433987dde9fa72656663e3014ce28947e664
Lennart Luttkus <lennart.luttkus@uni-a.de>
2024-02-13 10:05:00 +0100

    Refactor sim_config.py to add docstrings and improve code readability

--- robot_sf/sim_config.py
+++ robot_sf/sim_config.py
@@ -10,17 +10,28 @@ from robot_sf.sim.sim_config import SimulationSettings
 
 @dataclass
 class EnvSettings:
+    """
+    Data class to hold environment settings for a simulation.
+    """
     sim_config: SimulationSettings = SimulationSettings()
     lidar_config: LidarScannerSettings = LidarScannerSettings()
-    robot_config: Union[DifferentialDriveSettings, BicycleDriveSettings] = DifferentialDriveSettings()
+    robot_config: Union[DifferentialDriveSettings, BicycleDriveSettings] = \
+        DifferentialDriveSettings()
     map_pool: MapDefinitionPool = MapDefinitionPool()
 
     def __post_init__(self):
+        """
+        Check if any of the properties are not initialized (None) and raise an error if so.
+        """
         if not self.sim_config or not self.lidar_config \
                 or not self.robot_config or not self.map_pool:
             raise ValueError('Please make sure all properties are initialized!')
 
     def robot_factory(self) -> Union[DifferentialDriveRobot, BicycleDriveRobot]:
+        """
+        Factory method to create a robot instance based on the type of robot configuration provided.
+        :return: robot instance.
+        """
         if isinstance(self.robot_config, DifferentialDriveSettings):
             return DifferentialDriveRobot(self.robot_config)
         elif isinstance(self.robot_config, BicycleDriveSettings):

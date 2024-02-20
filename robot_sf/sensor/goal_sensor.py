063abf07c320faa7699d9987c3b8ad175870cb94
ll7 <sailingmaster92@gmx.de>
2024-02-20 09:10:45 +0100

    Refactor angle calculation function and rename it to calculate_trajectory_angle

--- robot_sf/sensor/goal_sensor.py
+++ robot_sf/sensor/goal_sensor.py
@@ -13,11 +13,13 @@ def norm_angle(angle: float) -> float:
     return (angle + np.pi) % (2 * np.pi) - np.pi
 
 
-def angle(p_1: Vec2D, p_2: Vec2D, p_3: Vec2D) -> float:
-    """Compute the difference between the linear projection of a vehicle at p1
+def calculate_trajectory_angle(p_1: Vec2D, p_2: Vec2D, p_3: Vec2D) -> float:
+    """
+    Compute the difference between the linear projection of a vehicle at p1
     moving straight towards the goal p2 and the straight trajectory from p2
     towards the next goal p3. For a smooth trajectory, the driving agent
-    has to steer such that the angle is about 0."""
+    has to steer such that the angle is about 0.
+    """
 
     v1_x, v1_y = p_2[0] - p_1[0], p_2[1] - p_1[1]
     v2_x, v2_y = p_3[0] - p_2[0], p_3[1] - p_2[1]
@@ -39,7 +41,7 @@ def target_sensor_obs(
         next_goal_pos: Union[Vec2D, None]) -> Tuple[float, float, float]:
     robot_pos, _ = robot_pose
     target_distance, target_angle = rel_pos(robot_pose, goal_pos)
-    next_target_angle = angle(robot_pos, goal_pos, next_goal_pos) \
+    next_target_angle = calculate_trajectory_angle(robot_pos, goal_pos, next_goal_pos) \
         if next_goal_pos is not None else 0.0
     return target_distance, target_angle, next_target_angle
 

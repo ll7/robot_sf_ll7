01fd4592c1753e3f6757dcf1fe7a35d32bcbe0c9
ll7 <sailingmaster92@gmx.de>
2024-03-06 15:39:48 +0100

    Add GlobalRoute class documentation

--- robot_sf/nav/map_config.py
+++ robot_sf/nav/map_config.py
@@ -65,6 +65,36 @@ class Obstacle:
 
 @dataclass
 class GlobalRoute:
+    """
+    A class to represent a global route.
+
+    Attributes
+    ----------
+    spawn_id : int
+        The id of the spawn point.
+    goal_id : int
+        The id of the goal point.
+    waypoints : List[Vec2D]
+        The waypoints of the route.
+    spawn_zone : Rect
+        The spawn zone of the route.
+    goal_zone : Rect
+        The goal zone of the route.
+
+    Methods
+    -------
+    __post_init__():
+        Validates the spawn_id, goal_id, and waypoints.
+    sections():
+        Returns the sections of the route.
+    section_lengths():
+        Returns the lengths of the sections.
+    section_offsets():
+        Returns the offsets of the sections.
+    total_length():
+        Returns the total length of the route.
+    """
+
     spawn_id: int
     goal_id: int
     waypoints: List[Vec2D]
@@ -72,6 +102,11 @@ class GlobalRoute:
     goal_zone: Rect
 
     def __post_init__(self):
+        """
+        Validates the spawn_id, goal_id, and waypoints.
+        Raises a ValueError if spawn_id or goal_id is less than 0 or if waypoints is empty.
+        """
+
         if self.spawn_id < 0:
             raise ValueError('Spawn id needs to be an integer >= 0!')
         if self.goal_id < 0:
@@ -81,14 +116,28 @@ class GlobalRoute:
 
     @property
     def sections(self) -> List[Tuple[Vec2D, Vec2D]]:
+        """
+        Returns the sections of the route as a list of tuples, where each tuple
+        contains two Vec2D objects
+        representing the start and end points of the section.
+        """
+
         return [] if len(self.waypoints) < 2 else list(zip(self.waypoints[:-1], self.waypoints[1:]))
 
     @property
     def section_lengths(self) -> List[float]:
+        """
+        Returns the lengths of the sections as a list of floats.
+        """
+
         return [dist(p1, p2) for p1, p2 in self.sections]
 
     @property
     def section_offsets(self) -> List[float]:
+        """
+        Returns the offsets of the sections as a list of floats.
+        """
+
         lengths = self.section_lengths
         offsets = []
         temp_offset = 0.0
@@ -99,6 +148,10 @@ class GlobalRoute:
 
     @property
     def total_length(self) -> float:
+        """
+        Returns the total length of the route as a float.
+        """
+
         return 0 if len(self.waypoints) < 2 else sum(self.section_lengths)
 
 

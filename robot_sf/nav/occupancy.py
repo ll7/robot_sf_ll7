605db51e22c601d28a455e62ff7280734541c0bc
ll7 <sailingmaster92@gmx.de>
2024-03-06 16:27:53 +0100

    Add circle intersection checks and continuous occupancy class

--- robot_sf/nav/occupancy.py
+++ robot_sf/nav/occupancy.py
@@ -11,10 +11,36 @@ Line2D = Tuple[Vec2D, Vec2D]
 
 @numba.njit(fastmath=True)
 def is_circle_circle_intersection(c_1: Circle2D, c_2: Circle2D) -> bool:
+    """
+    Checks if two circles intersect.
+
+    Parameters
+    ----------
+    c_1 : Circle2D
+        The first circle, represented as a tuple of the center coordinates and the radius.
+    c_2 : Circle2D
+        The second circle, represented as a tuple of the center coordinates and the radius.
+
+    Returns
+    -------
+    bool
+        True if the circles intersect, False otherwise.
+    """
+
+    # Extract the center coordinates and radius of the first circle
     center_1, radius_1 = c_1
+
+    # Extract the center coordinates and radius of the second circle
     center_2, radius_2 = c_2
+
+    # Calculate the square of the distance between the centers of the circles
     dist_sq = (center_1[0] - center_2[0])**2 + (center_1[1] - center_2[1])**2
+
+    # Calculate the square of the sum of the radii of the circles
     rad_sum_sq = (radius_1 + radius_2)**2
+
+    # The circles intersect if the square of the distance between their centers
+    # is less than or equal to the square of the sum of their radii
     return dist_sq <= rad_sum_sq
 
 
@@ -53,6 +79,31 @@ def is_circle_line_intersection(circle: Circle2D, segment: Line2D) -> bool:
 
 @dataclass
 class ContinuousOccupancy:
+    """
+    A class to represent a continuous occupancy.
+
+    Attributes
+    ----------
+    width : float
+        The width of the occupancy.
+    height : float
+        The height of the occupancy.
+    get_robot_coords : Callable[[], Vec2D]
+        A function to get the robot coordinates.
+    get_goal_coords : Callable[[], Vec2D]
+        A function to get the goal coordinates.
+    get_obstacle_coords : Callable[[], np.ndarray]
+        A function to get the obstacle coordinates.
+    get_pedestrian_coords : Callable[[], np.ndarray]
+        A function to get the pedestrian coordinates.
+    robot_radius : float, optional
+        The robot radius, by default 1.0.
+    ped_radius : float, optional
+        The pedestrian radius, by default 0.4.
+    goal_radius : float, optional
+        The goal radius, by default 1.0.
+    """
+
     width: float
     height: float
     get_robot_coords: Callable[[], Vec2D]
@@ -65,14 +116,38 @@ class ContinuousOccupancy:
 
     @property
     def obstacle_coords(self) -> np.ndarray:
+        """
+        Returns the obstacle coordinates.
+
+        Returns
+        -------
+        np.ndarray
+            The obstacle coordinates.
+        """
         return self.get_obstacle_coords()
 
     @property
     def pedestrian_coords(self) -> np.ndarray:
+        """
+        Returns the pedestrian coordinates.
+
+        Returns
+        -------
+        np.ndarray
+            The pedestrian coordinates.
+        """
         return self.get_pedestrian_coords()
 
     @property
     def is_obstacle_collision(self) -> bool:
+        """
+        Checks if there is a collision with an obstacle.
+
+        Returns
+        -------
+        bool
+            True if there is a collision with an obstacle, False otherwise.
+        """
         robot_x, robot_y = self.get_robot_coords()
         if not self.is_in_bounds(robot_x, robot_y):
             return True
@@ -86,6 +161,14 @@ class ContinuousOccupancy:
 
     @property
     def is_pedestrian_collision(self) -> bool:
+        """
+        Checks if there is a collision with a pedestrian.
+
+        Returns
+        -------
+        bool
+            True if there is a collision with a pedestrian, False otherwise.
+        """
         collision_distance = self.robot_radius
         ped_radius = self.ped_radius
         circle_robot = (self.get_robot_coords(), collision_distance)
@@ -97,13 +180,39 @@ class ContinuousOccupancy:
 
     @property
     def is_robot_robot_collision(self) -> bool:
+        """
+        ! Not Implemented !
+        """
         return False
 
     @property
     def is_robot_at_goal(self) -> bool:
+        """
+        Checks if the robot is at the goal.
+
+        Returns
+        -------
+        bool
+            True if the robot is at the goal, False otherwise.
+        """
         robot_circle = (self.get_robot_coords(), self.robot_radius)
         goal_circle = (self.get_goal_coords(), self.goal_radius)
         return is_circle_circle_intersection(robot_circle, goal_circle)
 
     def is_in_bounds(self, world_x: float, world_y: float) -> bool:
+        """
+        Checks if a point is within the bounds of the occupancy.
+
+        Parameters
+        ----------
+        world_x : float
+            The x-coordinate of the point.
+        world_y : float
+            The y-coordinate of the point.
+
+        Returns
+        -------
+        bool
+            True if the point is within the bounds of the occupancy, False otherwise.
+        """
         return 0 <= world_x <= self.width and 0 <= world_y <= self.height

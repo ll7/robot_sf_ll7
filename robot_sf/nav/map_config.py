0d40d7c52956c158b916ab4670577e8d7c565381
ll7 <sailingmaster92@gmx.de>
2024-03-06 14:31:36 +0100

    Add docs to Obstacle class to represent obstacles in map_config.py

--- robot_sf/nav/map_config.py
+++ robot_sf/nav/map_config.py
@@ -14,18 +14,48 @@ Rect = Tuple[Vec2D, Vec2D, Vec2D]
 
 @dataclass
 class Obstacle:
+    """
+    A class to represent an obstacle.
+
+    Attributes
+    ----------
+    vertices : List[Vec2D]
+        The vertices of the obstacle.
+    lines : List[Line2D]
+        The lines that make up the obstacle. This is calculated in the post-init method.
+    vertices_np : np.ndarray
+        The vertices of the obstacle as a numpy array. This is calculated in the post-init method.
+
+    Methods
+    -------
+    __post_init__():
+        Validates and processes the vertices to create the lines and vertices_np attributes.
+    """
+
     vertices: List[Vec2D]
     lines: List[Line2D] = field(init=False)
     vertices_np: np.ndarray = field(init=False)
 
     def __post_init__(self):
+        """
+        Validates and processes the vertices to create the lines and vertices_np attributes.
+        Raises a ValueError if no vertices are specified.
+        """
+
         if not self.vertices:
             raise ValueError('No vertices specified for obstacle!')
 
+        # Convert vertices to numpy array
         self.vertices_np = np.array(self.vertices)
+
+        # Create edges from vertices
         edges = list(zip(self.vertices[:-1], self.vertices[1:])) \
             + [(self.vertices[-1], self.vertices[0])]
-        edges = list(filter(lambda l: l[0] != l[1], edges)) # remove fake lines that are just points
+
+        # Remove fake lines that are just points
+        edges = list(filter(lambda l: l[0] != l[1], edges))
+
+        # Create lines from edges
         lines = [(p1[0], p2[0], p1[1], p2[1]) for p1, p2 in edges]
         self.lines = lines
 

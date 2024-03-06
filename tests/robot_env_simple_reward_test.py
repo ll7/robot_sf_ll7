6bd3342fa5c84cb2c6959e9eae87df35dfa65788
ll7 <sailingmaster92@gmx.de>
2024-03-06 14:18:31 +0100

    Add test for simple_reward function

---
+++ tests/robot_env_simple_reward_test.py
@@ -0,0 +1,18 @@
+from robot_sf.robot_env import simple_reward
+
+def test_simple_reward():
+    meta = {
+        "step": 0,
+        "episode": 0,
+        "step_of_episode": 0,
+        "is_pedestrian_collision": False,
+        "is_robot_collision": False,
+        "is_obstacle_collision": False,
+        "is_robot_at_goal": True,
+        "is_route_complete": False,
+        "is_timesteps_exceeded": False,
+        "max_sim_steps": 1000
+    }
+
+    reward = simple_reward(meta)
+    assert reward == 0.9999

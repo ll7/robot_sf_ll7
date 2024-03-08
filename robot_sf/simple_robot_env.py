75269cdf4eb5a2fdd828054938830740b379fc1b
ll7 <sailingmaster92@gmx.de>
2024-03-08 14:14:12 +0100

    Add action and observation space definitions and methods to SimpleRobotEnv

--- robot_sf/simple_robot_env.py
+++ robot_sf/simple_robot_env.py
@@ -5,23 +5,73 @@ to be natively compatible with gymnasium.
 """
 
 import gymnasium
+from gymnasium import spaces
+from gymnasium.utils import seeding
+
 
 class SimpleRobotEnv(gymnasium.Env):
     """
     A simple robot environment based on gymnasium.Env.
     """
 
-    def __init__(self):
+    def __init__(
+            self
+            env_config: EnvSettings = EnvSettings()
+            ):
         
-        self.action_space = gymnasium.spaces.Discrete(2)
+        self.info = {}
 
-        self.observation_space = gymnasium.spaces.Discrete(5)
+        # Action space
+        # TODO: Action space depends on the robot type? Differential drive or bicycle model?
+        self.action_space = self._define_action_space()
+        
+        # Observation Space
+        self.observation_space = self._define_observation_space()
+        
+    def _define_action_space(self):
+        """
+        Action Space:
+        - 2 actions: move forward, turn
+        """
+        return gymnasium.spaces.Discrete(2)
+
+
+    def _define_observation_space(self):
+        """
+        Observation Space:
+        - x, y, theta, v_x, v_y, omega
+        - goal_x, goal_y
+        - distance and angle to goal
+        - lidar observations
+        """
+        return gymnasium.spaces.Discrete(5)
+    
+    def _get_observation(self):
+        """
+        Get the current observation
+        """
+        return self.observation
+    
+    def _get_info(self):
+        """
+        Get the current info
+        """
+        return self.info
+
 
     def step(self, action):
         pass
 
-    def reset(self):
-        pass
+    def reset(self, seed=None):
+        # seed for self.np_random
+        super().reset(seed=seed)
+
+
+
+        
+        observation = self_get_observation()
+        info = self._get_info()
+        return observation, info
 
     def render(self, mode='human'):
         pass

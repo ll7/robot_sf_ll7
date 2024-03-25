663552be6e05f0a628691aaabb868fc240e8cfcc
ll7 <sailingmaster92@gmx.de>
2024-03-25 19:18:03 +0100

    refactor obstacle class

--- robot_sf/nav/map_config.py
+++ robot_sf/nav/map_config.py
@@ -4,66 +4,14 @@ define the map configuration
 import os
 import json
 import random
+
 from math import sqrt
 from typing import List, Union, Dict
 from dataclasses import dataclass, field
 
-import numpy as np
-
-from robot_sf.nav.gloabal_route import GlobalRoute
 from robot_sf.nav.nav_types import Vec2D, Line2D, Rect
-
-
-@dataclass
-class Obstacle:
-    """
-    A class to represent an obstacle.
-
-    Attributes
-    ----------
-    vertices : List[Vec2D]
-        The vertices of the obstacle.
-    lines : List[Line2D]
-        The lines that make up the obstacle. This is calculated in the post-init method.
-    vertices_np : np.ndarray
-        The vertices of the obstacle as a numpy array. This is calculated in the post-init method.
-
-    Methods
-    -------
-    __post_init__():
-        Validates and processes the vertices to create the lines and vertices_np attributes.
-    """
-
-    vertices: List[Vec2D]
-    lines: List[Line2D] = field(init=False)
-    vertices_np: np.ndarray = field(init=False)
-
-    def __post_init__(self):
-        """
-        Validates and processes the vertices to create the lines and vertices_np
-        attributes.
-        Raises a ValueError if no vertices are specified.
-        """
-
-        if not self.vertices:
-            raise ValueError('No vertices specified for obstacle!')
-
-        # Convert vertices to numpy array
-        self.vertices_np = np.array(self.vertices)
-
-        # Create edges from vertices
-        edges = list(zip(self.vertices[:-1], self.vertices[1:])) \
-            + [(self.vertices[-1], self.vertices[0])]
-
-        # Remove fake lines that are just points
-        edges = list(filter(lambda l: l[0] != l[1], edges))
-
-        # Create lines from edges
-        lines = [(p1[0], p2[0], p1[1], p2[1]) for p1, p2 in edges]
-        self.lines = lines
-
-        if not self.vertices:
-            print('WARNING: obstacle is just a single point that cannot collide!')
+from robot_sf.nav.gloabal_route import GlobalRoute
+from robot_sf.nav.obstacle import Obstacle
 
 
 @dataclass

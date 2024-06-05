f94222479880333ab93aa8e069dc8bf972f358cb
ll7 <sailingmaster92@gmx.de>
2024-06-05 08:56:57 +0000

    chore: Update devcontainer postCreate script

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 set -e
 
+git config --global --add safe.directory /workspaces/robot_sf_ll7
+
 ## This script is run after the devcontainer is created. It installs the necessary dependencies for the project.
 pwd
 git submodule update --init --recursive || { echo "Failed to update git submodules"; exit 1; }
+git config --global --add safe.directory /workspaces/robot_sf_ll7/fast-pysf
+
 pip install -r ./requirements.txt || { echo "Failed to install requirements from ./requirements.txt"; exit 1; }
 pip install -r ./fast-pysf/requirements.txt || { echo "Failed to install requirements from ./fast-pysf/requirements.txt"; exit 1; }
 pip install -e . || { echo "Failed to install the current directory as a package"; exit 1; }

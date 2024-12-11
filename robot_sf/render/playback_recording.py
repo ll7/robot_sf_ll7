9fc8313608993cee4578017f4e19455a50fb139f
ll7 <32880741+ll7@users.noreply.github.com>
2024-12-11 15:58:40 +0100

    feat: enhance load_states function with validation and add load_states_and_record_video function

--- robot_sf/render/playback_recording.py
+++ robot_sf/render/playback_recording.py
@@ -14,7 +14,7 @@ logger = loguru.logger
 
 def load_states(filename: str) -> List[VisualizableSimState]:
     """
-    load a list of states from a file with pickle
+    load a list of states from a file with pickle `*.pkl` format
     """
     # Check if the file is empty
     if os.path.getsize(filename) == 0:
@@ -25,6 +25,18 @@ def load_states(filename: str) -> List[VisualizableSimState]:
     with open(filename, "rb") as f:  # rb = read binary
         states, map_def = pickle.load(f)
     logger.info(f"Loaded {len(states)} states")
+
+    # Verify `states` is a list of VisualizableSimState
+    if not all(isinstance(state, VisualizableSimState) for state in states):
+        logger.error(f"Invalid states loaded from {filename}")
+        raise TypeError(f"Invalid states loaded from {filename}")
+
+    # Verify `map_def` is a MapDefinition
+    if not isinstance(map_def, MapDefinition):
+        logger.error(f"Invalid map definition loaded from {filename}")
+        logger.error(f"map_def: {type(map_def)}")
+        raise TypeError(f"Invalid map definition loaded from {filename}")
+
     return states, map_def
 
 
@@ -46,3 +58,22 @@ def load_states_and_visualize(filename: str):
     """
     states, map_def = load_states(filename)
     visualize_states(states, map_def)
+
+
+def load_states_and_record_video(state_file: str, vide_save_path: str, video_fps: float = 10):
+    """
+    load a list of states from a file and record a video
+    """
+    logger.info(f"Loading states from {state_file}")
+    states, map_def = load_states(state_file)
+    sim_view = SimulationView(
+        map_def=map_def,
+        caption="RobotSF Recording",
+        record_video=True,
+        video_path=vide_save_path,
+        video_fps=video_fps,
+    )
+    for state in states:
+        sim_view.render(state)
+
+    sim_view.exit_simulation()  # to write the video file

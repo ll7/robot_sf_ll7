bb393ee92101f0794d958abdb9862b72849b5f13
ll7 <sailingmaster92@gmx.de>
2024-12-12 15:38:49 +0100

    feat: integrate simulator initialization and visualization in BaseEnv class

--- robot_sf/gym_env/base_env.py
+++ robot_sf/gym_env/base_env.py
@@ -8,8 +8,9 @@ from loguru import logger
 from gymnasium import Env
 from typing import List
 
-from robot_sf.render.sim_view import VisualizableSimState
 from robot_sf.gym_env.env_config import EnvSettings
+from robot_sf.render.sim_view import VisualizableSimState, SimulationView
+from robot_sf.sim.simulator import init_simulators
 
 
 class BaseEnv(Env):
@@ -46,6 +47,29 @@ class BaseEnv(Env):
                 self.video_fps = video_fps
                 self._set_video_fps()
 
+        self.map_def = env_config.map_pool.choose_random_map()
+
+        # Initialize simulator with a random start position
+        self.simulator = init_simulators(
+            env_config,
+            self.map_def,
+            random_start_pos=True,
+            peds_have_obstacle_forces=peds_have_obstacle_forces,
+        )[0]
+
+        if debug or record_video:
+            self.sim_ui = SimulationView(
+                scaling=10,
+                map_def=self.map_def,
+                obstacles=self.map_def.obstacles,
+                robot_radius=env_config.robot_config.radius,
+                ped_radius=env_config.sim_config.ped_radius,
+                goal_radius=env_config.sim_config.goal_radius,
+                record_video=record_video,
+                video_path=video_path,
+                video_fps=video_fps,
+            )
+
     def _set_video_fps(self):
         """
         This methods sets a default value for the video_fps attribute if it was not provided.
@@ -54,3 +78,21 @@ class BaseEnv(Env):
         if video_fps is None:
             video_fps = 1 / self.env_config.sim_config.time_per_step_in_secs
             logger.info(f"Video FPS not provided, setting to {video_fps}")
+
+    def step(self, action=None):
+        """
+        Advances the simulation by one step.
+        Does not take any action.
+        Returns only dummy values.
+        """
+        # todo: the simulator in this configuration requires a robot action
+        # Instead the robot should be redesigned to not require a pedestrian.
+        self.simulator.step_once()
+
+        obs = None
+        reward = None
+        terminal = None
+        truncated = None
+        info = None
+
+        return (obs, reward, terminal, truncated, info)

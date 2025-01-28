0858490cf271daa0f242d7c99edc941fbf05bedf
ll7 <32880741+ll7@users.noreply.github.com>
2025-01-28 17:50:13 +0100

    feat: enhance BaseEnv initialization with environment configuration and reward function

--- robot_sf/gym_env/base_env.py
+++ robot_sf/gym_env/base_env.py
@@ -7,7 +7,12 @@ Provides common functionality for all robot-based environments.
 # import datetime
 # import pickle
 
+from typing import Callable
+from loguru import logger
+
 from gymnasium import Env
+from robot_sf.gym_env.reward import simple_reward
+from robot_sf.gym_env.env_config import EnvSettings
 
 # from robot_sf.gym_env.env_config import BaseEnvSettings
 # from robot_sf.render.sim_view import (
@@ -20,12 +25,31 @@ from gymnasium import Env
 class BaseEnv(Env):
     """Base environment class that handles common functionality."""
 
+    def __init__(
+        self,
+        env_config: EnvSettings = EnvSettings(),
+        reward_func: Callable[[dict], float] = simple_reward,
+        debug: bool = False,
+        recording_enabled: bool = False,
+        record_video: bool = False,
+        video_path: str = None,
+        video_fps: float = None,
+    ):
+        super().__init__()
 
-def __init__():
-    super().__init__()
+        # Environment configuration details
+        self.env_config = env_config
 
-def render():
-    pass
+        # Set video FPS if not provided
+        if video_fps is None:
+            video_fps = 1 / self.env_config.sim_config.time_per_step_in_secs
+            logger.info(f"Video FPS not provided, setting to {video_fps}")
 
-def step():
-    pass
+        # Extract first map definition; currently only supports using the first map
+        self.map_def = env_config.map_pool.choose_random_map()
+
+    def render():
+        pass
+
+    def step():
+        pass

24e8f43e037ec5436314b83d6c9c2586f04a66cd
ll7 <32880741+ll7@users.noreply.github.com>
2025-02-21 14:40:38 +0100

    test: add unit test for pedestrian and robot spawn in obstacle scenario

---
+++ test_pygame/test_spawn_in_obstacle.py
@@ -0,0 +1,30 @@
+"""
+Test what happens if pedestrian and robot spwan in an obstacle.
+"""
+
+from robot_sf.gym_env.robot_env_with_pedestrian_obstacle_forces import (
+    RobotEnvWithPedestrianObstacleForces,
+)
+from robot_sf.nav.svg_map_parser import convert_map
+from loguru import logger
+
+def test_spawn_in_obstacle():
+    logger.info("Testing Spawn in Obstacle")
+    map_def = convert_map("maps/svg_maps/test_spawn_in_obstacle.svg")
+    logger.debug(f"type map_def: {type(map_def)}")
+    env = RobotEnvWithPedestrianObstacleForces(map_def=map_def, debug=True)
+    logger.info("created environment")
+    env.reset()
+    for _ in range(1000):
+        rand_action = env.action_space.sample()
+        _, _, done, _, _ = env.step(rand_action)
+        env.render()
+        if done:
+            env.reset()
+    env.close()
+
+
+if __name__ == "__main__":
+    logger.info("Testing Pedestrian and Obstacle forces")
+    test_spawn_in_obstacle()
+    logger.info("All tests passed")
